2b1a64a1aee12a95121c4524794772f77c356af5
Anhelina Daineka <dnk.angelina@gmail.com>
2023-08-07 22:32:03 +0300

    Modified main swiper, adaptive tablet parts til service

---
+++ src/js/header.js
@@ -0,0 +1,28 @@
+var prevScrollpos = window.scrollY;
+        window.onscroll = function () {
+            let currentScrollPos = window.scrollY;
+            if(window.innerWidth > 700){
+                if (prevScrollpos > currentScrollPos) {
+                    document.getElementById("navbar").style.position = "fixed";
+                    document.getElementById("navbar").style.top = "0";
+                } else {
+                    document.getElementById("navbar").style.top = "-110px";
+                }
+                if(currentScrollPos > 100){
+                    document.getElementById("navbar").style.backgroundColor = "rgba(233, 130, 131, 0.9)";
+                }
+                else{
+                    document.getElementById("navbar").style.backgroundColor = "rgba(37, 43, 73, 0)";
+                }
+                prevScrollpos = currentScrollPos;
+            }
+            else{
+                if(currentScrollPos > 100){
+                    document.getElementById("navbar").style.position = "fixed";
+                    document.getElementById("navbar").style.backgroundColor = "rgba(233, 130, 131, 0.9)";
+                }
+                else{
+                    document.getElementById("navbar").style.backgroundColor = "rgba(37, 43, 73, 0)";
+                }
+            }
+        }
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,7 +1,7 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
-const swiper = new Swiper('.swiper-container', {
+let swiper = new Swiper('.swiper-container', {
     modules: [Navigation, Pagination],
     slidesPerView: 1,
     spaceBetween: 10,
@@ -12,3 +12,52 @@ const swiper = new Swiper('.swiper-container', {
     },
 });
 
+
+let swiper2 = new Swiper('.main-page-swiper', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
+
+
+const pag = document.querySelectorAll('.swiper-pagination-bullet');
+const text = document.querySelectorAll('.pag-cont-item__text');
+const container = document.querySelectorAll('.main-page-swiper')
+let indexOfPag, indexOfPagClick;
+
+
+container.forEach((elem, index) => {
+    elem.addEventListener('mouseover', function (){
+        text.forEach((text, index) => {
+            text.classList.remove('active-pag');
+        });
+        pag.forEach((button, index) =>{
+            if(button.classList.contains('swiper-pagination-bullet-active')){
+                indexOfPag = index;
+                text.forEach((text, index) => {
+                    if(index == indexOfPag){
+                        text.classList.add('active-pag');
+                    }
+                });
+            }
+        });
+    });
+    pag.forEach((button, index) =>{
+        button.addEventListener('click', function () {
+            text.forEach((text, index) => {
+                text.classList.remove('active-pag');
+            });
+            indexOfPagClick = index;
+            text.forEach((text, index) => {
+                if(index == indexOfPagClick){
+                    text.classList.add('active-pag');
+                }
+            });
+        });
+    });
+});
